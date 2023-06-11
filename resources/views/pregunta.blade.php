47defc68b1eccef1fc188978d7c43f59e3f03918
YasifJoaquin <yasif.19070002@itsmotul.edu.mx>
2023-06-11 12:02:46 -0600

    responsividad de la ventana previa a crear cartel

--- resources/views/pregunta.blade.php
+++ resources/views/pregunta.blade.php
@@ -24,19 +24,19 @@
         </div> --}}
 
         <div class="fixed top-0 left-0 w-screen h-screen flex items-center justify-center">
-            <div class="max-sm:w-5/6 sm:w-5/6 md:w-3/6 h-auto m-auto rounded-xl bg-amber-900 flex flex-col items-center">
-                <h2 class="text-center font-bold mt-4 mb-4 tracking-wide text-black sm:text-lg max-sm:px-3 sm:px-3 md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl">Argh, ¿Has perdido un tesoro? o ¿Has encontrado un tesoro?</h2>
-                <div class="z-40 flex justify-between w-full">
+            <div class="max-sm:w-5/6 sm:w-4/6 max-sm:h-auto sm:h-auto md:w-3/6 md:h-3/6 lg:w-3/5 lg:h-4/6 m-auto rounded-xl bg-amber-900 flex flex-col items-center">
+                <h2 class="text-center font-bold mt-4 mb-4 tracking-wide text-black sm:text-xl max-sm:px-3 sm:px-3 sm:mb-4 sm:mt-4 md:text-2xl md:mb-8 md:mt-6 lg:text-3xl lg:mb-10 lg:mt-8 xl:text-4xl ">Argh, ¿Has perdido un tesoro? o ¿Has encontrado un tesoro?</h2>
+                <div class="z-40 flex justify-between w-full sm:mb-2 md:mb-4 lg:mb-6">
                     <a href="{{ route('cperdido') }}" class="w-auto h-auto rounded-r-full bg-red-600 flex items-center justify-center hover:bg-blue-800 border-2 border-black" name="icono 1">
-                        <img src="{{ asset('src/mapa.png')}}" alt="Icono" class="max-sm:w-20 sm:h-20 sm:p-2 max-sm:p-2 mx-4 mb-1">
+                        <img src="{{ asset('src/mapa.png')}}" alt="Icono" class="max-sm:w-20 sm:h-24 max-sm:p-2 sm:p-4 md:w-28 md:h-24 lg:w-32 lg:h-32 xl:w-36 xl:h-36 2xl:w-44 2xl:h-40 mx-4 mb-1">
                     </a>
                     <a href="{{ route('cencontrado') }}" class="w-auto h-auto rounded-l-full bg-red-600 flex items-center justify-center hover:bg-blue-800 border-2 border-black" name="icono 2">
-                        <img src="{{ asset('src/pala.png')}}" alt="Icono" class="max-sm:w-20 sm:h-20 sm:p-2 max-sm:p-2 mx-4 mb-1">
+                        <img src="{{ asset('src/pala.png')}}" alt="Icono" class="max-sm:w-20 sm:h-24 max-sm:p-2 sm:p-4 md:w-28 md:h-24 lg:w-32 lg:h-32 xl:w-36 xl:h-36 2xl:w-44 2xl:h-40 mx-4 mb-1">
                     </a>
                 </div>
                 <div class="z-50 flex justify-between w-full mb-5">
-                    <p class="text-left text-black sm:text-lg md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl px-4 mt-2 tracking-wide">He perdido un tesoro</p>
-                    <p class="text-right text-black sm:text-lg md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl px-4 mt-2 tracking-wide">He encontrado un tesoro</p>
+                    <p class="text-left text-black sm:text-lg md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl pl-2 mt-2 tracking-wide">He perdido un tesoro</p>
+                    <p class="text-right text-black sm:text-lg md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl pr-2 mt-2 tracking-wide">He encontrado un tesoro</p>
                 </div>
             </div>
         </div>
